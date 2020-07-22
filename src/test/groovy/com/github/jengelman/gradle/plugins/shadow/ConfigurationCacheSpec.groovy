a587b77e631f0a008e7640aba7dd0571af6931a7
Benedikt Ritter <beneritter@gmail.com>
2020-07-22 15:40:06 +0200

    Extend test to use the application plugin

--- src/test/groovy/com/github/jengelman/gradle/plugins/shadow/ConfigurationCacheSpec.groovy
+++ src/test/groovy/com/github/jengelman/gradle/plugins/shadow/ConfigurationCacheSpec.groovy
@@ -5,8 +5,39 @@ import com.github.jengelman.gradle.plugins.shadow.util.PluginSpecification
 class ConfigurationCacheSpec extends PluginSpecification {
 
     def "supports configuration cache"() {
+        given:
+        repo.module('shadow', 'a', '1.0')
+                .insertFile('a.properties', 'a')
+                .insertFile('a2.properties', 'a2')
+                .publish()
+
+        file('src/main/java/myapp/Main.java') << """
+            package myapp;
+            public class Main {
+               public static void main(String[] args) {
+                   System.out.println("TestApp: Hello World! (" + args[0] + ")");
+               }
+            }
+        """.stripIndent()
+
+        buildFile << """
+            apply plugin: 'application'
+
+            mainClassName = 'myapp.Main'
+            
+            dependencies {
+               compile 'shadow:a:1.0'
+            }
+            
+            runShadow {
+               args 'foo'
+            }
+        """.stripIndent()
+
+        settingsFile << "rootProject.name = 'myapp'"
+
         when:
-        runner.withArguments('--configuration-cache', 'shadowJar').buildAndFail()
+        runner.withArguments('--configuration-cache', 'shadowJar').build()
         def result = runner.withArguments('--configuration-cache', 'shadowJar').build()
 
         then:
